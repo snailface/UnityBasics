eadbded72f2d08cdc007000272658bf3d12ad2ed
Hasala Perera <Hasala@Hasalas-Mac-mini.local>
2014-05-07 13:41:17 +0530

    Added a New Tutorial - Reinstating Objects

---
+++ Unity Help Manual - Snailface/ReinstateObjectToOriginalLocation/Assets/Reinstate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reinstate : MonoBehaviour {
+	private Vector3 InitialPosition;
+	// Use this for initialization
+	void Start () {
+		InitialPosition = transform.position; 
+		rigidbody.isKinematic = true;
+		rigidbody.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			rigidbody.useGravity = true;
+			rigidbody.isKinematic = false;
+		}
+
+		if(transform.position.y < 0.5){
+			transform.position = InitialPosition;
+			rigidbody.useGravity = false;
+			rigidbody.isKinematic = true;
+		}
+
+	}
+}
